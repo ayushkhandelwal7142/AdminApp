6e8b1d5b923beae1a6c86cdc34f430d5449b46be
Ayush Khandelwal <akstudios610@gmail.com>
2023-01-10 16:46:18 +0530

    ui design changed

--- app/src/main/java/com/akstudios/adminapp/loginScreen/LoginActivity.kt
+++ app/src/main/java/com/akstudios/adminapp/loginScreen/LoginActivity.kt
@@ -1,12 +1,11 @@
 package com.akstudios.adminapp.loginScreen
 
+import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.view.View
+import android.widget.*
 import com.akstudios.adminapp.MainActivity
 import com.akstudios.adminapp.R
 import com.google.firebase.auth.FirebaseAuth
@@ -14,10 +13,12 @@ import com.google.firebase.auth.FirebaseAuth
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var firebaseAuth: FirebaseAuth
-    private lateinit var btnLogin: Button
+    private lateinit var btnLogin: ImageView
     private lateinit var btnSignUp: TextView
     private lateinit var etEmailAddress: EditText
     private lateinit var etPassword: EditText
+    private lateinit var progressDialog: ProgressDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_actiity)
@@ -27,6 +28,7 @@ class LoginActivity : AppCompatActivity() {
         etEmailAddress = findViewById(R.id.etEmailAddress)
         etPassword = findViewById(R.id.etPassword)
         btnSignUp = findViewById(R.id.btnSignUp)
+        progressDialog = ProgressDialog(this)
 
         btnLogin.setOnClickListener {
             login()
@@ -44,14 +46,21 @@ class LoginActivity : AppCompatActivity() {
             Toast.makeText(this, "Email/password cannot be empty", Toast.LENGTH_SHORT).show()
             return
         } else {
+            progressDialog.apply {
+                setTitle("Logging in")
+                setMessage("Please wait...")
+                show()
+            }
             firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this){
                 if(it.isSuccessful){
+                    progressDialog.dismiss()
                     Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show()
                     val intent = Intent(this, MainActivity::class.java)
                     startActivity(intent)
                     finish()
                 }
                 else {
+                    progressDialog.dismiss()
                     Toast.makeText(this, "Authentication Failed", Toast.LENGTH_SHORT).show()
                 }
             }
